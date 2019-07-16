d8d1c9b40844eeada56e768427f38e25dcdfbf74
王杰 <wangjie@icaremgt.com>
2019-07-16 17:48:47 +0800

    懒汉式单例

---
+++ src/singleton/LazySingleton.java
@@ -0,0 +1,21 @@
+package singleton;
+
+/**
+ * 懒汉式
+ *
+ * @author 王杰
+ * @date 2019-07-16 17:42
+ */
+
+public class LazySingleton {
+    //    private byte[] data = new byte[2014];
+    private static LazySingleton instance = null;
+    private LazySingleton(){
+    }
+    public static LazySingleton getInstance() {
+        if (instance==null) {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
