a0f56027e119744145c1541e0d15de53a51d43cc
王杰 <wangjie@icaremgt.com>
2019-07-16 17:39:46 +0800

    饿汉式单例

---
+++ src/singleton/HungrySingleton.java
@@ -0,0 +1,22 @@
+package singleton;
+
+/**
+ * 饿汉式单例
+ *
+ * @author 王杰
+ * @date 2019-07-16 17:25
+ */
+public final class HungrySingleton {
+//    private byte[] data = new byte[2014];
+    private static HungrySingleton instance = new HungrySingleton();
+
+    /**
+     *  私有构造
+     */
+    private HungrySingleton() {
+
+    }
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
+}
